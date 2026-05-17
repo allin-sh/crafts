46ceabf76844fe1ce4b0b0061ec551789a663249
gaki2 <qudrkr0815@gmail.com>
2026-05-17 16:21:03 +0900

    design: change left dock font-family

--- .ladle/FlickingGrid.tsx
+++ .ladle/FlickingGrid.tsx
@@ -1,6 +1,6 @@
 'use client';
 
-import React from 'react';
+import type React from 'react';
 import { useCallback, useEffect, useMemo, useRef, useState } from 'react';
 
 interface FlickeringGridProps extends React.HTMLAttributes<HTMLDivElement> {
